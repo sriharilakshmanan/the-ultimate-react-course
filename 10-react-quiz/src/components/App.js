3aef4e253ceea364a5d8e20fb98e31d885a5df54
Sri Hari Lakshmanan <sriharilakshmanan05@gmail.com>
2023-11-23 07:36:29 +0530

    init: initial commit for the react-quiz app

---
+++ 10-react-quiz/src/components/App.js
@@ -0,0 +1,10 @@
+import DateCounter from "./DateCounter";
+function App() {
+    return (
+        <div className="app">
+            <DateCounter />
+        </div>
+    );
+}
+
+export default App;
